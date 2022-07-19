a9bddf818d2f4c9c477551e4639a48f69358ca83
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-19 15:48:39 -0500

    Completed assignment

--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -5,11 +5,18 @@ function Mole(props) {
 
   const { whack, dens } = props
   // console.log(key)
-  return (
-    <div className="den">
-      { dens && <img src={MoleIcon} className="Mole" alt="Mole" onClick={whack}/> }
-    </div>
-  )
+
+  if(dens) {
+    return (
+      <div className="den">
+        <img src={MoleIcon} className="Mole" alt="Mole" onClick={whack}/>
+      </div>
+    )
+  } else {
+    return (
+      <div className="den"></div>
+    )
+  }
 }
 
 export default Mole
